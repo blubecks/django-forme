9fbf6429d007aae6625482fae4ded7b978a2f67e
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2014-02-14 23:23:48 +0100

    Cleanup.

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -10,10 +10,12 @@ from forme.parser import FormeParser
 
 def test_node_factory():
     default_node_args = ('default', 'default', [])
+    # All valid tags should return Node subclass
     for tag in FormeParser.valid_tags:
         assert tag in nodes.node_classes
         assert isinstance(nodes.node_factory(tag, *default_node_args),
                           template.Node)
 
+    # Unknown tag
     with pytest.raises(KeyError):
         nodes.node_factory('42_fish')
