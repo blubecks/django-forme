df5c81865eb0147aaa988267ac7814edab561273
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2014-02-17 22:05:59 +0100

    Added validator for allowed child nodes.

--- forme/nodes.py
+++ forme/nodes.py
@@ -5,33 +5,58 @@ from django import template
 
 
 class FormeNodeBase(template.Node):
+    valid_child_nodes = ()
+    all_forme_nodes = ()
+
     def __init__(self, target, action, nodelist):
         self.target = target
         self.action = action
         self.nodelist = nodelist
 
+        self.validate_child_nodes()
+
+        # Nodes without targets are default templates.
+        self.default = not target
+
     def render(self, context):
         pass
 
+    def validate_child_nodes(self):
+        child_nodes = self.get_nodes_by_type(self.all_forme_nodes)
+        # First node in list is node itself.
+        child_nodes.pop(0)
 
-class FormeNode(FormeNodeBase):
-    pass
+        if not child_nodes:
+            return
 
+        if self.valid_child_nodes:
+            invalid = lambda node: not isinstance(node, self.valid_child_nodes)
+            child_nodes = filter(invalid, child_nodes)
 
-class FieldsetNode(FormeNodeBase):
-    pass
-
-
-class RowNode(FormeNodeBase):
-    pass
-
-
-class LabelNode(FormeNodeBase):
-    pass
+        if child_nodes:
+            msg = ("Node {0} can\'t contain following nodes: {1}"
+                   .format(self.__class__, child_nodes))
+            raise template.TemplateSyntaxError(msg)
 
 
 class FieldNode(FormeNodeBase):
     pass
+
+
+class FieldErrorsNode(FormeNodeBase):
+    pass
+
+
+class LabelNode(FormeNodeBase):
+    valid_child_nodes = (FieldNode, FieldErrorsNode)
+
+
+class RowNode(FormeNodeBase):
+    valid_child_nodes = (FieldNode, LabelNode, FieldErrorsNode)
+
+
+class FieldsetNode(FormeNodeBase):
+    valid_child_nodes = (RowNode,) + RowNode.valid_child_nodes
 
 
 class HiddenFieldsNode(FormeNodeBase):
@@ -42,8 +67,9 @@ class ErrorsNode(FormeNodeBase):
     pass
 
 
-class FieldErrorsNode(FormeNodeBase):
-    pass
+class FormeNode(FormeNodeBase):
+    valid_child_nodes = ((HiddenFieldsNode, ErrorsNode, FieldsetNode)
+                         + FieldsetNode.valid_child_nodes)
 
 
 node_classes = {
@@ -56,6 +82,7 @@ node_classes = {
     'errors': ErrorsNode,
     'fielderrors': FieldErrorsNode,
 }
+FormeNodeBase.all_forme_nodes = tuple(node_classes.values())
 
 
 def node_factory(tag_name, *args, **kwargs):
--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,11 +1,21 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
+import itertools as I
 import pytest
 from django import template
 
 from forme import nodes
 from forme.parser import FormeParser
+
+
+def pytest_generate_tests(metafunc):
+    if not metafunc.cls:
+        return
+
+    # called once per each test function
+    funcarglist = metafunc.cls().params[metafunc.function.__name__]
+    metafunc.parametrize('template_string', funcarglist, ids=funcarglist)
 
 
 def test_node_factory():
@@ -19,3 +29,64 @@ def test_node_factory():
     # Unknown tag
     with pytest.raises(KeyError):
         nodes.node_factory('42_fish')
+
+
+class TestChildNodes:
+    tags = ("field fielderrors label row fieldset"
+            " errors hiddenfields".split())
+
+    nodes_tag_map = dict(zip(nodes.node_classes.values(),
+                             nodes.node_classes.keys()))
+
+    def top_template(self, string):
+        return ("{{% load forme %}}{{% forme using %}}{string}{{% endforme %}}"
+                .format(string=string))
+
+    def tag_template(self, tag, string=''):
+        return ("{{% {tag} using %}}{string}{{% end{tag} %}}"
+                .format(tag=tag, string=string))
+
+    def valid_tags(self, tag):
+        return [self.nodes_tag_map[node] for node in
+                nodes.node_classes[tag].valid_child_nodes]
+
+    def invalid_tags(self, tag):
+        return set(self.tags) - set(self.valid_tags(tag))
+
+    @property
+    def params(self):
+        valid_templates = []
+        invalid_templates = []
+
+        for tag in self.tags:
+            # invalid_templates = []
+            valid_templates.append(self.tag_template(tag))
+            for valid in self.valid_tags(tag):
+                valid_templates.append(
+                    self.tag_template(tag, self.tag_template(valid)))
+
+            for invalid in self.invalid_tags(tag):
+                invalid_templates.append(
+                    self.tag_template(tag, self.tag_template(invalid)))
+
+        return {
+            'test_valid': valid_templates,
+            'test_invalid': invalid_templates,
+        }
+
+    def test_valid(self, template_string):
+        tmpl = self.top_template(template_string)
+        try:
+            template.Template(tmpl)
+        except template.TemplateSyntaxError as e:
+            pytest.fail("Should be valid template: {0}."
+                        " Exception was {1}.".format(tmpl, e))
+
+    def test_invalid(self, template_string):
+        tmpl = self.top_template(template_string)
+        try:
+            template.Template(tmpl)
+        except template.TemplateSyntaxError:
+            pass
+        else:
+            pytest.fail("Shouldn't be valid template: {0}".format(tmpl))
