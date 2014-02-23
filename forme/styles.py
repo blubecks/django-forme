eadfc05a755f993f2ec2f433dcbada653a771cef
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2014-02-23 21:43:10 +0100

    Added slice notation to style object.

--- forme/styles.py
+++ forme/styles.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+
+from django.utils.datastructures import SortedDict
 
 Variant = namedtuple('Variant', 'tag target')
 
@@ -9,31 +11,38 @@ class Default:
     pass
 
 
-class Style(dict):
+class Style(object):
+    def __init__(self):
+        self._data = defaultdict(SortedDict)
+
+    def __contains__(self, key):
+        key = self._normalize_key(key)
+        if isinstance(key.target, slice):
+            return key.tag in self._data
+        else:
+            return key.tag in self._data and key.target in self._data[key.tag]
+
     def __getitem__(self, key):
         key = self._normalize_key(key)
-        try:
-            return super(Style, self).__getitem__(key)
-        except KeyError:
-            pass
+        if isinstance(key.target, slice):
+            return self._data[key.tag]
 
-        if key.tag != Default:
-            alter_key = Variant(key.tag, Default)
-            try:
-                return super(Style, self).__getitem__(alter_key)
-            except KeyError:
-                pass
-
-        msg = 'Template for tag {0} is missing.'.format(key.tag)
-        raise KeyError(msg)
+        variants = self._data[key.tag]
+        if key.target != Default and key.target in variants:
+            return variants[key.target]
+        else:
+            return variants[Default]
 
     def __setitem__(self, key, value):
         key = self._normalize_key(key)
-        return super(Style, self).__setitem__(key, value)
+        self._data[key.tag][key.target] = value
 
     def __delitem__(self, key):
         key = self._normalize_key(key)
-        return super(Style, self).__delitem__(key)
+        if isinstance(key.target, slice):
+            del self._data[key.tag]
+        else:
+            del self._data[key.tag][key.target]
 
     @classmethod
     def _normalize_key(cls, key):
--- tests/test_styles.py
+++ tests/test_styles.py
@@ -15,3 +15,18 @@ def test_indexing():
 
     with pytest.raises(KeyError):
         assert style['invalid tag']
+
+
+def test_ellipsis():
+    style = styles.Style()
+    style['forme'] = 'Foo'
+    style['forme', 'test'] = 'Bar'
+    style['forme', 'py'] = 'Fubar'
+    assert len(style['forme', :]) == 3
+
+    del style['forme', 'py']
+    assert len(style['forme', :]) == 2
+
+    del style['forme', :]
+    assert 'forme' not in style
+
